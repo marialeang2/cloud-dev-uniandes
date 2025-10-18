ef6ee803c8cb8c1e0b524c73c97fe01870db922a
Pablo Pedreros <p.pedreros@uniandes.edu.co>
2025-10-18 18:33:18 -0500

    video editing

--- app/tasks/video_tasks.py
+++ app/tasks/video_tasks.py
@@ -4,6 +4,7 @@ from time import sleep
 from uuid import UUID
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from moviepy import ImageClip, VideoFileClip, CompositeVideoClip, vfx
 
 from app.core.celery_app import celery_app
 from app.core.config import settings
@@ -24,7 +25,7 @@ def process_video_task(video_id: str, temp_file_path: str):
     Steps:
     1. Update status to 'processing'
     2. Validate video with FFprobe
-    3. Process video (TO_DO: cutting, resizing, adding banner)
+    3. Process video (cutting, resizing, adding banner)
     4. Move to processed folder
     5. Update status to 'processed' or 'failed'
     """
@@ -48,9 +49,24 @@ def process_video_task(video_id: str, temp_file_path: str):
         video.duration_seconds = int(metadata['duration'])
         db.commit()
         
-        # TO_DO: Process video (cutting, adding banner, etc.)
-        # For now, just copy the file as-is to processed folder
-        sleep(15)
+        # Process video (cutting, adding banner, watermark and resizing.)
+        videoclip = VideoFileClip("../../" + video.file_path)
+
+        if video.duration_seconds > 30:
+            videoclip = videoclip.with_end(30)
+            watermark = ImageClip("../res/ANBLogo-1920x1080.png").set_start(2.5).set_duration(30).set_pos(("center","center")).with_position(("center", 0.25), relative=True)
+            outro = ImageClip("../res/ANBLogo-1920x1080.png").set_start(32.5).set_duration(3.5).set_pos(("center","center")).with_position(("center", 0.25), relative=True)
+        else:
+            watermark = ImageClip("../res/ANBLogo-1920x1080.png").set_start(2.5).set_duration(video.duration_seconds).set_pos(("center","center")).with_position(("center", 0.25), relative=True)
+            outro = ImageClip("../res/ANBLogo-1920x1080.png").set_start(video.duration_seconds + 3.5).set_duration(3.5).set_pos(("center","center"))
+
+        intro = ImageClip("../res/ANBLogo-1920x1080.png").set_start(0).set_duration(3.5).set_pos(("center","center"))
+
+        videoclip_with_intro = CompositeVideoClip([intro, videoclip.with_effects([vfx.CrossFadeIn(1)]), watermark.with_effects(vfx.CrossFadeIn(1)), outro.with_effects(vfx.CrossFadeIn(1))])
+
+        videoclip_with_intro = videoclip_with_intro.without_audio().with_effects([vfx.resize(height=1080)])
+
+        videoclip_with_intro.write_videofile("../../storage/processed/" + str(video.id))
         
         # Move file from uploads to processed folder
         temp_path = Path(temp_file_path)
